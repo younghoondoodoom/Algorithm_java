14313974492311267f0b00d2aa4cc327ffa85fb6
ChoiYoungHoon <dudgns2865@naver.com>
2022-05-10 21:36:01 +0900

    stack

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,74 +5,51 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StackClass stack = new StackClass();
-        int N = Integer.parseInt(br.readLine());
+        BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
 
+        int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
-            String command = br.readLine();
-            if (command.contains("push")) {
-                String spt[] = command.split(" ");
-                stack.push(Integer.parseInt(spt[1]));
-            } else if (command.contains("pop")) {
-                bw.write(stack.pop()+"\n");
-            } else if (command.contains("size")) {
-                bw.write(stack.size()+"\n");
-            } else if (command.contains("empty")) {
-                bw.write(stack.empty()+"\n");
-            } else if (command.contains("top")) {
-                bw.write(stack.top()+"\n");
-            } else {
-                continue;
+            String s = br.readLine();
+            String[] s1 = s.split(" ");
+            for (String s2 : s1) {
+                StackClass stack = new StackClass(s2);
+                wr.write(stack.reverse()+" ");
             }
+            wr.write("\n");
         }
 
-        bw.flush();
-        bw.close();
+        wr.flush();
+        wr.close();
         br.close();
-
     }
 
 }
 
-class StackClass {
+class StackClass{
 
-    static Stack<Integer> stack = new Stack<>();
+    Stack<String> stack = new Stack<>();
 
-    public void push(int n) {
-        stack.add(n);
-    }
-
-    public int pop() {
-        if (stack.isEmpty()){
-            return -1;
-        }
-        return stack.pop();
-    }
-
-    public int size() {
-        return stack.size();
-    }
-
-    public int empty() {
-        if (stack.isEmpty()) {
-            return 1;
-        } else {
-            return 0;
+    public StackClass(String str) {
+        String[] strings = str.split("");
+        for (String string : strings) {
+            stack.push(string);
         }
     }
 
-    public int top() {
-        if (stack.isEmpty()){
-            return -1;
+    public String reverse() {
+        ArrayList<String> lis = new ArrayList<>();
+        int size = stack.size();
+        for (int i = 0; i < size; i++) {
+            lis.add(stack.pop());
         }
-        return stack.peek();
+        return String.join("", lis);
     }
 
 }
