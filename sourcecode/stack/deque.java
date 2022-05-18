62f3963aed07cc085c421d4be4d34f70c7c8af38
ChoiYoungHoon <dudgns2865@naver.com>
2022-05-18 23:06:43 +0900

    deque

--- sourcecode/stack/deque.java
+++ sourcecode/stack/deque.java
@@ -1,7 +1,5 @@
-package org.example;
-
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.BuffredWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
