488b801316b2d0add556f2cf59a572f53b394657
ChoiYoungHoon <dudgns2865@naver.com>
2022-08-27 12:17:08 +0900

    카드 구매하기2

---
+++ sourcecode/algorithm/dp/카드 구매하기2.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] card = new int[n + 1];
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            card[i] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i < n + 1; i++) {
+            dp[i] = Integer.MAX_VALUE;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j] + card[j]);
+            }
+        }
+
+        wr.write(dp[n] + "");
+        wr.flush();
+        wr.close();
+        br.close();
+    }
+
+}
+
