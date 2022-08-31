05824eecb875618de9062da6007b44e693a86fbc
ChoiYoungHoon <dudgns2865@naver.com>
2022-08-31 23:11:02 +0900

    재곱 수의 합

---
+++ sourcecode/algorithm/dp/제곱수의 합.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+
+        for (int i = 1; i <= n; i++) {
+            dp[i] = i;
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        wr.write(dp[n] + "");
+
+        wr.flush();
+        wr.close();
+        br.close();
+
+    }
+
+}
+
