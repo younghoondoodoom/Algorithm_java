a557665b87226dac40f143f632b6f14197c73d3a
ChoiYoungHoon <dudgns2865@naver.com>
2022-08-31 20:58:01 +0900

    가장 긴 증가하는 부분 수열

---
+++ sourcecode/algorithm/dp/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,48 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter wr = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+
+        dp[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = i - 1; j >= 0; j--) {
+                if (arr[i] > arr[j] && dp[i] <= dp[j]) {
+                    dp[i] = dp[j] + 1;
+                }
+            }
+        }
+
+        int result = Arrays.stream(dp).max().getAsInt();
+
+        wr.write(result + "");
+        wr.flush();
+        wr.close();
+        br.close();
+
+    }
+
+}
+
